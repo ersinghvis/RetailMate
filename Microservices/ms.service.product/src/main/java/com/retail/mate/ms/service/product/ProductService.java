696942ca792bb0aa478b0a78f30f2c6d1e816bba
visha1 <Vishal.1@amdocs.com>
2025-04-26 21:44:45 +0530

    Config added

--- Microservices/ms.service.product/src/main/java/com/retail/mate/ms/service/product/ProductService.java
+++ Microservices/ms.service.product/src/main/java/com/retail/mate/ms/service/product/ProductService.java
@@ -2,12 +2,14 @@ package com.retail.mate.ms.service.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Application {
+@EnableDiscoveryClient
+public class ProductService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ProductService.class, args);
 	}
 
 }
